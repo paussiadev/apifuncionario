24ee48db19d3b133c39ad0436413b4c1235d061f
Paussia <paussia.costa@portalsesisp.org.br>
2024-03-22 10:01:52 -0300

    apifuncionario

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+using apifuncionario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apifuncionario.Context;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions options) : base (options)
+    {
+    }
+
+    public DbSet<Funcionario> Funcionarios {get;set;}
+    public DbSet<Setor> Setores {get;set;}
+}
---
+++ Modelo de Funcionario/Setor.cs
@@ -0,0 +1,7 @@
+namespace apifuncionario.Models;
+
+public class Setor
+{
+    public int SetorId {get;set;}
+    public string? Nome {get;set;}
+}
